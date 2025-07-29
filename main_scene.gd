28a3b940be573041422754d46baaf2c05e7a566a
Joshua Patterson <joshua.l.patterson7@gmail.com>
2025-07-29 11:32:03 -0500

    Add more styling and clean up code

--- main_scene.gd
+++ main_scene.gd
@@ -7,21 +7,43 @@ var is_running := false
 var is_rest_period := false
 var time_remaining := TOTAL_TIME
 
+var grey_stylebox := StyleBoxFlat.new()
+var green_stylebox := StyleBoxFlat.new()
+
 
 func format_time(t: float) -> String:
-	var min := str(int(floor(t / 60.0)))
-	var sec := str(int(floor(t)) % 60)
-	if int(sec) < 10:
-		sec = "0" + sec
+	var minute := str(int(floor(t / 60.0)))
+	var second := str(int(floor(t)) % 60)
+	if int(second) < 10:
+		second = "0" + second
 	
-	if min == "0":
-		return sec
-	return min + ":" + sec
+	if minute == "0":
+		return second
+	return minute + ":" + second
 
 
 func update_timer(new_time: float) -> void:
 	time_remaining = new_time
 	$CanvasLayer/TimerLbl.text = format_time(time_remaining)
+
+
+func set_to_working() -> void:
+	update_timer(TOTAL_TIME)
+	is_rest_period = false
+	$CanvasLayer/BackgroundPanel.add_theme_stylebox_override("panel", grey_stylebox)
+	$CanvasLayer/StatusLbl.text = "Working!"
+
+
+func set_to_resting() -> void:
+	update_timer(REST_TIME)
+	is_rest_period = true
+	$CanvasLayer/BackgroundPanel.add_theme_stylebox_override("panel", green_stylebox)
+	$CanvasLayer/StatusLbl.text = "Resting!"
+
+
+func _ready() -> void:
+	grey_stylebox.bg_color = Color.DIM_GRAY
+	green_stylebox.bg_color = Color.WEB_GREEN
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -31,11 +53,9 @@ func _process(delta: float) -> void:
 		if time_remaining <= 0.0:
 			$AlarmPlayer.play()
 			if is_rest_period:
-				update_timer(TOTAL_TIME)
-				is_rest_period = false
+				set_to_working()
 			else:
-				update_timer(REST_TIME)
-				is_rest_period = true
+				set_to_resting()
 
 
 func _on_play_btn_pressed() -> void:
@@ -48,4 +68,4 @@ func _on_play_btn_pressed() -> void:
 
 func _on_stop_btn_pressed() -> void:
 	is_running = false
-	update_timer(TOTAL_TIME)
+	set_to_working()
